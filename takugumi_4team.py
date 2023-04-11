3d73d92f520458523ee3df5a3e28a8e879eec58d
CitronStudio <90139266+CitronStudio@users.noreply.github.com>
2023-04-11 15:33:51 +0900

    takugumi_4team.py を更新

--- takugumi_4team.py
+++ takugumi_4team.py
@@ -2,13 +2,13 @@ import random
 import console
 
 # 参加者を設定。mは4の倍数にすること
-m = 36
+m = 32
 
 # 1チーム当たりのメンバー　=　全参加者 ÷4
 team_mem = m // 4
 
 #動作確認の為の試行回数カウント
-Rcount = -1
+retry_count = 0
 
 # m人の参加者を作成
 players = list(range(1, m + 1))
@@ -53,30 +53,27 @@ while True:
     match_history = []
 
     console.clear()
-    Rcount += 1
-    print(f"{m}人・1チーム{team_mem}人(Retry Count : {Rcount})")
+    print(f"{m}人・1チーム{team_mem}人(Retry Count : {retry_count})")
     print()
 
-    retry = 0
+    loop_count = 0
     i = 0
-    while i < 5:
-        round_output = []
-        round_output.append(f"{i + 1}ゲーム目")
-
+    while i < 5:   
         # チームをリセット
         team_a_copy = team_a.copy()
         team_b_copy = team_b.copy()
         team_c_copy = team_c.copy()
         team_d_copy = team_d.copy()
-
+        
+        round_output = []
         current_round_matches = []
         for j in range(team_mem):
-            table_output = f" {j + 1}番卓 : "
+            table_output = f"第{i + 1}試合\t{j + 1}番卓"
             match = generate_match(team_a_copy, team_b_copy, team_c_copy, team_d_copy, match_history)
             
             # 対戦カードを作成出来なかった場合
             if match is None:
-                retry += 1
+                loop_count += 1
                 break
             player1, player2, player3, player4 = match
             table_output += f"\t{player1}\t{player2}\t{player3}\t{player4}"
@@ -85,14 +82,17 @@ while True:
         else:
             for output in round_output:
                 print(output)
+            print()
             match_history.extend(current_round_matches)
-            retry = 0
+            loop_count = 0
             i += 1
             
         # リトライが一定数を超えたら、もう一度全て最初からやり直す。リトライ数は挙動を見ながら変えてもよい
-        if retry > 100:
+        if loop_count > 100:
             break   
     
     # 5戦目まで作成が成功した場合、プログラムを終了する
     if i == 5:
         break
+        
+    retry_count += 1
